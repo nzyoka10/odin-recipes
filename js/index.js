bae89886831b7c02f37e53ed405f9d1c45d0e820
nzyoka10 <mnzyokaeric@gmail.com>
2023-09-18 09:57:45 +0300

    js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 // learning JavaScript
 console.log('Running Js Today......');
 
-// variables
-let name = 'Nzyoka10'; 
-console.log(name);
 
 /**
- * rules of naming variables
+ *!   Variables
+ **  Rules of naming variables
  * 1. cannot be a reserved word
  * 2. should be  meaningful and unique
- * 3. cannot start with a number
+ * 3. cannot start with a number (1name)
  * 4. should not contain a space or hyphen (-)
  * 5. Are case sensitive 
  */
+let name = 'Nzyoka10'; 
+console.log(name); 
+
+let firstName = 'Nzyoka10';
+let lastName = 'eric';
+
+console.log(lastName); 
+
+
