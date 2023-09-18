5c107dbc9df2b24de1e11c7c8884fed1a240030e
nzyoka10 <mnzyokaeric@gmail.com>
2023-09-18 14:08:01 +0300

    js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ console.log('Running Js Today......');
 /**
  *!   Variables
  **  Rules of naming variables
+ *
  * 1. cannot be a reserved word
  * 2. should be  meaningful and unique
  * 3. cannot start with a number (1name)
@@ -19,4 +20,49 @@ let lastName = 'eric';
 
 console.log(lastName); 
 
+/**
+ * ! Constants in JavaScript
+ * used to declear none changing varaibles in JavaScript
+ */
 
+const interestRate = 0.2;
+//interestRate = 1;
+console.log(interestRate);
+
+
+/**
+ **  Primitive or Value data types
+* ---- Strings, Number, Boolean, Undefined, and Null
+ */
+let sName = 'Nzyoka'; // string literal 
+let age = 30; //number literal
+let isApproved = false; //boolean literal (True Or False)
+let fName = undefined; 
+let selectColor = null; 
+let city = 'makueni';
+
+
+// JS is dynamic languange
+// reference data types 
+/**
+ * object
+ * array\
+ * function
+ */
+
+// object person
+let person = {
+    name: 'Nzyoka',
+    age: 30,
+    city: 'Makueni'
+};
+
+//array 
+let fruits = ["apple", "orange", "mango", "Dunda"];
+
+// function
+function greet(name, age, city) {
+    console.log('Hello ' + name + ' ' + age + ' ' + city);
+}
+
+greet( name, age, city);
